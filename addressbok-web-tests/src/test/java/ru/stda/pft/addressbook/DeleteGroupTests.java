d67d83bd5f8a1d03d43d9c09bc0b459cd4fb8ad0
epichuzhkina <epichuzhkina@beeline.ru>
2023-07-09 14:08:44 +0300

    добавление DeleteGroupTests

---
+++ addressbok-web-tests/src/test/java/ru/stda/pft/addressbook/DeleteGroupTests.java
@@ -0,0 +1,60 @@
+package ru.stda.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DeleteGroupTests {
+    private WebDriver wd;
+    private JavascriptExecutor js;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        js = (JavascriptExecutor) wd;
+    }
+
+    @Test
+    public void testDeleteGroupTests() throws Exception {
+        wd.get("http://localhost:8080/addressbook/group.php");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        wd.findElement(By.linkText("groups")).click();
+        wd.findElement(By.name("selected[]")).click();
+        wd.findElement(By.name("delete")).click();
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+   }
